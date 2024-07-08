e6acdf44965ea9ed4dd6220fb5f14ff02ad67432
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2024-07-08 14:23:05 +0530

    created register user routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,4 +18,32 @@ const loginUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export { loginUser };
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exist");
+	}
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+	if (user) {
+		generateToken(res, user._id);
+
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid User Credentials");
+	}
+});
+
+export { loginUser, registerUser };
