ed9034cc5c3aa0f23c54f510ca2b4925627d9916
SRI-HARIPRIYAN <sris.haripriyan@gmail.com>
2024-07-27 23:04:48 +0530

    deployment fix -1

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 const BASE_URL = "";
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://eco-cart-il9d.onrender.com";
 const PRODUCTS_URL = "/api/products";
 const USERS_URL = "/api/users";
 const ORDERS_URL = "/api/orders";
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		proxy: {
-			"/api": "http://localhost:5000/",
-			"/upload": "http://localhost:5000",
+			"/api": "https://eco-cart-il9d.onrender.com/",
+			"/upload": "https://eco-cart-il9d.onrender.com",
 		},
 	},
 });
